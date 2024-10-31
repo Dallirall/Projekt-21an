bd678a5938036da7e3064788b8954716cf637ce6
Matilda <matildafahlstrom90@hotmail.com>
2024-10-31 13:56:39 +0100

    Add project files.

---
+++ Projekt 21an/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Projekt_21an;
+
+namespace spel21an
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tärningssimulator.RunDiceSimulator();
+        }
+    }
+}
---
+++ Projekt 21an/tärningssimulator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Projekt_21an
+{
+    public static class Tärningssimulator
+    {
+        public static void RunDiceSimulator()
+        {
+            Console.WriteLine("Hur många tärningar vill du rulla? ");
+            int valtAntalTärningar = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många sidor ska varje tärning ha? ");
+            int valtAntalTärningssidor = int.Parse(Console.ReadLine());
+
+            Random slumpgenerator = new Random();
+            Console.WriteLine("Kastar... ");
+            for (int i = 0; i < valtAntalTärningar; i++)
+            {
+                Console.WriteLine($"Tärning {(i + 1)}: {slumpgenerator.Next(1, (valtAntalTärningssidor + 1))}");
+            }
+
+
+            Console.ReadKey();
+            
+
+        }
+    }
+}
