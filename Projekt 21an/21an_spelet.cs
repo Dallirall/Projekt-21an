c1215b13ae796e1bf301ba7f5fb9426de9ea69a0
Matilda <matildafahlstrom90@hotmail.com>
2024-11-06 11:52:58 +0100

    Ska just ändra till att göra en ny class av 21an-spelet, så bara menyn osv finns i main

---
+++ Projekt 21an/21an_spelet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_21an
+{
+    public class _21an_spelet
+    {
+        private string regler = "I 21:an kommer du att spela mot datorn och försöka tvinga datorn att få över 21 poäng. Både du och datorn får poäng genom att dra kort, varje kort är värt 1 – 10 poäng. När spelet börjar dras två kort till både dig och datorn. Därefter får du dra hur många kort som du vill tills du är nöjd med din totalpoäng, du vill komma så nära 21 som möjligt utan att få mer än 21 poäng. När du inte vill dra fler kort så kommer datorn att dra kort tills den har mer eller lika många poäng som dig.\\r\\n\\r\\nDu vinner om datorn får mer än totalt 21 poäng när den håller på att dra kort. Datorn vinner om den har mer poäng än dig när spelet är slut så länge som datorn inte har mer än 21 poäng. Om det skulle bli lika i poäng så vinner datorn. Om du får mer än 21 poäng när du drar kort så har du förlorat.\\n\\n\""
+        public string Regler { get; }
+
+
+
+}
+    
+}
